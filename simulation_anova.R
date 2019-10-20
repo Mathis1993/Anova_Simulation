c434021fd054e27bad4435e0244c74374cc5ac18
Mathis <m_eric01@uni-muenster.de>
2019-10-20 18:53:38 +0200

    add different group sizes

--- simulation_anova.R
+++ simulation_anova.R
@@ -1,24 +1,29 @@
 #Create an anova dataset:
-# - n_samples (int): Number of samples per group.
-# - means (num or vector of num): Mean(s) of the dv in the groups. Specify one for all groups or one for each group.
-# - sds (num or vector of num): Standard deviation(s) of the dv in the groups. Specify one for all groups or one for each group.
+# - n_samples (int or vector of int): Number of samples per group. Specify one for all groups or one for each group (as a vector).
+# - means (num or vector of num): Mean(s) of the dv in the groups. Specify one for all groups or one for each group (as a vector).
+# - sds (num or vector of num): Standard deviation(s) of the dv in the groups. Specify one for all groups or one for each group (as a vector).
 #The dv will be i.i.d normally distributed with the respective means and sds.
-anova_dataset <- function(n_samples=10000, means=5, sds=5){
+anova_dataset <- function(n_samples=c(1000, 1000, 1000), means=c(5,10,15), sds=c(5,5,5)){
   #coerce to vectors
+  n_samples <- c(n_samples)
   means <- c(means)
   sds <-  c(sds)
   
+  #create scores: draw from normal distribtion parametrized according to each mean-/sd-value as many
+  #times as specified
+  scores <- mapply(function(n_sample, mean, sd)rnorm(n=n_sample,mean=mean,sd=sd),n_samples,means,sds)
+  scores <- unlist(scores)
+  scores <-as.vector(scores)
+  
   #get amount of groups
-  n_groups <- max(length(means), length(sds))
+  n_groups <- max(length(n_samples), length(means), length(sds))
+  #create group labels
+  groups <- mapply(function(n_sample,n_group)rep(n_group,each=n_sample),n_samples,1:n_groups)
+  groups <- unlist(groups)
+  groups <- as.vector(groups)
   
-  #draw scores (draws iteratively over the mean/sd vector)
-  scores <- rnorm(n=n_samples*n_groups, mean=means, sd=sds)
-  
-  #make groups
-  groups <- rep(1:n_groups, times=n_samples)
-  
-  #make ids
-  ids <- seq(1,n_groups*n_samples)
+  #create ids
+  ids <- seq(1,length(scores))
   
   #combine into dataset
   anova_dataset <- data.frame(id=as.factor(ids), group=as.factor(groups), score=scores)
@@ -38,3 +43,5 @@ aov_het <- aov(score ~ group, data=het)
 summary(aov_hom)
 summary(aov_het)
 
+#different group sizes
+sizes <- anova_dataset(n_samples=c(1000,500,700), means=c(5,5,10), sds=2)
