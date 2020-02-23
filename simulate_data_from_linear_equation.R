1dcde2530a79549c7b62e1d8b6dadbae345f69eb
Mathis <m_eric01@uni-muenster.de>
2020-02-23 15:48:58 +0100

    moved files on helpful concepts to separate directory

--- simulate_data_from_linear_equation.R
+++
@@ -1,9 +0,0 @@
-#Iwelche Daten simulieren
-X <- rnorm(100, mean = 5, sd = 2)
-
-#b0, b1 festlegen, z.B. b0=1, b1=2 --> Erwartungswerte in Abhängikeit von X generieren
-expected_y <- 1 + 2 * X
-
-#Einen "beobachteten" Wert pro erwartetem Wert ziehen: Aus Verteilung mit erwartetem Wert als MW und einer SD 
-#--> erwarteter Wert mit Noise
-y <- rnorm(100, mean = expected_y, sd = 4)
